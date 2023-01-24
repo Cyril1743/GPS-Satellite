4bee9dbd7911af8ea3e3debc27b33d9b7bf5cf7a
Cyril1743 <75587967+Cyril1743@users.noreply.github.com>
2023-01-23 21:35:51 -0600

    added event listener function

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,14 +1,14 @@
 //varibles for maps
 var map;
-var lat;
-var long;
+var lat = 39.060150;
+var long = -95.647301;
 var searchInput = $("#search-bar").val()
-var searchButton = $("#search-button")
+var searchButton = $("#button-addon2")
 
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 39.060150, lng: -95.647301 },
+    center: { lat: lat, lng: long },
     zoom: 4,
   });
 }
@@ -16,5 +16,5 @@ function initMap() {
 window.initMap = initMap;
 
 searchButton.on("click", function(){
-    
+    var search = searchInput.replace(" ", "%20")
 })
