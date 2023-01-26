aab328380cf16a6689583789d27d569091ed439b
dleatonsalinas <daniellesalinas721@gmail.com>
2023-01-25 21:04:21 -0600

    update sizing with Tailwind

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,22 +1,40 @@
-//varibles for map 
-// var map
-// var lat
-// var long
-// function initMap() {
-//     map = new google.maps.Map($("map")), {
-//         center : { lat: 37.0902, lng: 150.644},
-//         zoom: 2
-//     }
-// }
-// window.initMap = initMap;
-// export {lat, long}
-let map;
+//varibles for maps
+var map;
+var lat = 39.060150;
+var long = -95.647301;
+var searchInput = $("#default-search")
+var searchButton = $("#search")
+var geocode
+
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -37.0902, lng: 95.7129 },
-    zoom: 8,
+    center: { lat: lat, lng: long },
+    zoom: 4,
   });
 }
 
 window.initMap = initMap;
+
+searchButton.on("click", function (event) {
+  event.preventDefault()
+  var searchValue = searchInput.val()
+  console.log(searchValue)
+  var search = searchValue.replace(" ", "%20")
+  var search = search.replace("&", "%26")
+  var requestUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + search + "&key=AIzaSyBAc-vn35rr4MOnhvddt4qmf0EicQ-PUtM"
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      var results = data
+      console.log(results)
+      lat = results.results[0].geometry.location.lat
+      long = results.results[0].geometry.location.lng
+      map = new google.maps.Map(document.getElementById("map"), {
+        center:{lat: lat, lng: long},
+        zoom: 8,
+      })
+    })
+})
