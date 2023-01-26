55539af320077a945ce423ff2db507a3cdbd67fd
Cyril1743 <75587967+Cyril1743@users.noreply.github.com>
2023-01-25 20:04:28 -0600

    Added search button functionallity

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,8 +2,9 @@
 var map;
 var lat = 39.060150;
 var long = -95.647301;
-var searchInput = $("#search-bar").val()
-var searchButton = $("#button-addon2")
+var searchInput = $("#default-search")
+var searchButton = $("#search")
+var geocode
 
 
 function initMap() {
@@ -15,6 +16,25 @@ function initMap() {
 
 window.initMap = initMap;
 
-searchButton.on("click", function(){
-    var search = searchInput.replace(" ", "%20")
+searchButton.on("click", function (event) {
+  event.preventDefault()
+  var searchValue = searchInput.val()
+  console.log(searchValue)
+  var search = searchValue.replace(" ", "%20")
+  var search = search.replace("&", "%26")
+  var requestUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + search + "&key=AIzaSyBAc-vn35rr4MOnhvddt4qmf0EicQ-PUtM"
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      var results = data
+      console.log(results)
+      lat = results.results[0].geometry.location.lat
+      long = results.results[0].geometry.location.lng
+      map = new google.maps.Map(document.getElementById("map"), {
+        center:{lat: lat, lng: long},
+        zoom: 8,
+      })
+    })
 })
