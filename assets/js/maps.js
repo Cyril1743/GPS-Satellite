f187093bc239affb53681c4320249c5bbbb8a3e1
Alan Hornbaker <alanhornbaker@gmail.com>
2023-01-26 18:46:48 -0600

    updated script.js after switching from nasa API to spectator API

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,11 +1,10 @@
 //varibles for maps
 var map;
-var lat = 39.060150;
+var lat = 39.06015;
 var long = -95.647301;
-var searchInput = $("#default-search")
-var searchButton = $("#search")
-var geocode
-
+var searchInput = $("#default-search");
+var searchButton = $("#search");
+var geocode;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -17,24 +16,29 @@ function initMap() {
 window.initMap = initMap;
 
 searchButton.on("click", function (event) {
-  event.preventDefault()
-  var searchValue = searchInput.val()
-  console.log(searchValue)
-  var search = searchValue.replace(" ", "%20")
-  var search = search.replace("&", "%26")
-  var requestUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + search + "&key=AIzaSyBAc-vn35rr4MOnhvddt4qmf0EicQ-PUtM"
+  event.preventDefault();
+  var searchValue = searchInput.val();
+  console.log(searchValue);
+  var search = searchValue.replace(" ", "%20");
+  var search = search.replace("&", "%26");
+  var requestUrl =
+    "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+    search +
+    "&key=AIzaSyBAc-vn35rr4MOnhvddt4qmf0EicQ-PUtM";
   fetch(requestUrl)
     .then(function (response) {
-      return response.json()
+      return response.json();
     })
     .then(function (data) {
-      var results = data
-      console.log(results)
-      lat = results.results[0].geometry.location.lat
-      long = results.results[0].geometry.location.lng
+      var results = data;
+      console.log(results);
+      lat = results.results[0].geometry.location.lat;
+      long = results.results[0].geometry.location.lng;
       map = new google.maps.Map(document.getElementById("map"), {
-        center:{lat: lat, lng: long},
+        center: { lat: lat, lng: long },
         zoom: 8,
-      })
-    })
-})
+      });
+    });
+});
+
+export var { lat, long };
